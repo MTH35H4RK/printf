fc126af79412d8dc79353e3f0cc10ca32c2dcf43
bouarichaKarim <bouaaricha.karim@gmai.com>
2023-03-27 02:56:39 -0700

    Update including header file in _printf.c

--- _printf.c
+++ _printf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 
 	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
 	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
+ih		return (-1);
 	if (!format[i])
 		return (0);
 	for (i = 0; format && format[i]; i++)
