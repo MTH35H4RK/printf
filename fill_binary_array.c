1ff6f714991d74ac370a836bd8c4bc85fb69e7c5
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-03-27 06:47:11 -0700

    Add fill_binary_array.c

--- fill_binary_array.c
+++ fill_binary_array.c
@@ -6,6 +6,7 @@
  * @int_in: input number
  * @isneg: if input number is negative
  * @limit: size of the binary
+ *
  * Return: number of chars printed.
  */
 char *fill_binary_array(char *binary, long int int_in, int isneg, int limit)
