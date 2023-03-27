0a2d0fc97caa6606d2077f1d883e0779789fbeaf
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-03-27 06:48:21 -0700

    update ev_print_func.c

--- ev_print_func.c
+++ ev_print_func.c
@@ -3,6 +3,7 @@
  * ev_print_func - returns the amount of identifiers.
  * @s: argument indentifier
  * @index: index of argument identifier.
+ *
  * Return: amount of identifiers .
  */
 int ev_print_func(const char *s, int index)
