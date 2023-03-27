6998cfa23e20a9c7ab725a9460bb4f12015b6fe2
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-03-27 06:49:07 -0700

    Update fill_hex_array

--- fill_hex_array.c
+++ fill_hex_array.c
@@ -7,6 +7,7 @@
  * @isupp: integer that determines if the hexadecimal array is
  * in uppercase or lowercase letter.
  * @limit: size of hex
+ *
  * Return: binary array.
  */
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit)
